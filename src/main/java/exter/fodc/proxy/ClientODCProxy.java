fea5e437f3025214da65bc4f2a97897c0c39dd38
EXTER <exter7@gmail.com>
2013-12-25 11:47:19 -0400

    Cleaned up unused imports and other warnings.

--- src/main/java/exter/fodc/proxy/ClientODCProxy.java
+++ src/main/java/exter/fodc/proxy/ClientODCProxy.java
@@ -1,9 +1,5 @@
 package exter.fodc.proxy;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.client.MinecraftForgeClient;
-
 public class ClientODCProxy extends CommonODCProxy
 {
   public void Init()
